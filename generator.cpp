96d6c830f15ca122fd08f453d01d7e97ea6473c0
Ivan Ulitin <ulitin.ivan3004@mail.ru>
2023-03-31 22:38:20 +0300

    сделал rc4 и поправил линейную

--- generator.cpp
+++ generator.cpp
@@ -7,7 +7,10 @@
 #include <map>
 #include <bitset>
 #include <boost/dynamic_bitset.hpp>
+#include <numeric>
 
+
+const int BIT_AMOUNT = 32;
 
 void print_vector(std::vector<int> some_vec)
 {
@@ -90,10 +93,10 @@ std::string lc_help_message =
 
 std::string add_help_message =
 "Для аддитивного генератора необходимо ввести параметры следующим "
-"образом: '/i:m,j,k,x_0,x_1,...,x_n' - где:"
+"образом: '/i:m,k,j,x_0,x_1,...,x_n' - где:"
 "\n m - модуль,"
-"\n j - некоторый индекс (j > k >= 1),"
-"\n k - некоторый индекс (j > k >= 1),"
+"\n k - некоторый младший индекс (j > k >= 1),"
+"\n j - некоторый старший индекс (j > k >= 1),"
 "\n x_0,x_1,...,x_n - вводимая последовательность\n";
 
 std::string lfsr_help_message =
@@ -104,20 +107,36 @@ std::string lfsr_help_message =
 
 std::string fp_help_message =
 "Для пятипараметрического генератора необходимо ввести параметры следующим "
-"образом: '/i:q_1,q_2,q_3,w,s' - где:"
+"образом: '/i:s,q_1,q_2,q_3,w' - где:"
 "\n q_1,q_2,q_3 - коэффициенты пятипараметрического метода,"
 "\n w - размерность чисел в битах (целое положительное число)"
-"\n s - начальное состояние регистра (целое положительное число)";
+"\n p - начальное состояние регистра (целое положительное число)";
+
+std::string nfsr_help_message =
+"Для нелинейной комбинации РСЛОС необходимо ввести параметры следующим "
+"образом: '/i:r1,c1,r2,c2,r3,c3' - где:"
+"\n r1 - начальное состояние регистра 1 (32 числа '0' или '1' без разделителей),"
+"\n c1 - коэффициенты многочлена 1 (32 числа '0' или '1' без разделителей),"
+"\n r2 - начальное состояние регистра 2,"
+"\n c2 - коэффициенты многочлена 2,"
+"\n r3 - начальное состояние регистра 3,"
+"\n c3 - коэффициенты многочлена 3\n";
+
+// rc4_method
+std::string rc4_help_message =
+"Для метода RC4 необходимо ввести параметры следующим "
+"образом: '/i:x1,x2,...,x256' - где:"
+"\n x1,... - первые 256 начальных значений\n";
 
 std::string other_gen_help_message = "Данный генератор ещё не готов или ошибка "
 "в названии генератора!\n";
-
 
 std::string find_generate_method(std::string method)
 {
     // функция для нахождения названия метода в 
     // списке допустимых названий методов
-    std::vector<std::string> method_list = {"lc", "add", "5p", "lfsr"};
+    std::vector<std::string> method_list = {"lc", "add", "5p", "lfsr", "nfsr",
+                                            "rc4"};
     for (int i = 0; i < method_list.size(); i++)
     {
         if (method == method_list[i])
@@ -152,6 +171,14 @@ void show_help_message(std::string g="", bool h=false)
     {
         std::cout << std::endl << fp_help_message << std::endl;        
     }
+    else if (g == "nfsr")
+    {
+        std::cout << std::endl << nfsr_help_message << std::endl;        
+    }
+    else if (g == "rc4")
+    {
+        std::cout << std::endl << rc4_help_message << std::endl;        
+    }
     else if (g != "")
     {
         std::cout << std::endl << other_gen_help_message << std::endl;
@@ -170,6 +197,66 @@ void show_progress(int i, int n)
 }
 
 
+std::vector<int> get_prime_factors(int n)
+{
+    std::vector<int> divs;
+    while (n % 2 == 0)
+    {
+        divs.push_back(2);
+        n = n/2;
+    }
+    for (int i = 3; i <= sqrt(n); i = i + 2)
+    {
+        while (n % i == 0)
+        {
+            divs.push_back(i);
+            n = n/i;
+        }
+    }
+    if (n > 2)
+        divs.push_back(n);
+    
+    return divs;
+}
+
+bool check_linear_params(int m, int a, int c)
+{
+    bool is_pass = false;
+
+    // 1-е условие
+    if (std::__gcd(m, c) != 1)
+    {
+        return false;
+    }
+
+    // 2-е условие
+    std::vector<int> m_divisors = get_prime_factors(m);
+    int a_min_one = a - 1;
+    for (int i = 0; i < m_divisors.size(); i++)
+    {
+        int p = m_divisors[i];
+        if (a_min_one % p == 0)
+        {
+            is_pass = true;
+            break;
+        }
+    }
+
+    // 3-е условие
+    if (m % 4 == 0)
+    {
+        if (a_min_one % 4 == 0 && is_pass) return true;
+        return false;
+    }
+    else
+    {
+        if (is_pass) return true;
+    }
+
+    return false;
+}
+
+
 int linear_congruent_method(int n, std::vector<int> params, std::string f)
 {
     // реализация линейного конгруэнтного метода
@@ -177,6 +264,14 @@ int linear_congruent_method(int n, std::vector<int> params, std::string f)
     int a = params[1];
     int c = params[2];
     int last_x = params[3];
+
+    if (!check_linear_params(m, a, c))
+    {
+        std::cout << std::endl;
+        std::cout << "Генерация отменена - свойства теоремы не выполняются! "
+                     "Измените входные параметры.";
+        return 1;
+    }
 
     std::ofstream output_file;
     output_file.open(f);
@@ -196,9 +291,9 @@ int additive_method(int n, std::vector<int> params, std::string f)
     // реализация аддитивного метода
     int m = params[0];
     params.erase(params.begin());
-    int j = params[1];
+    int k = params[1];
     params.erase(params.begin());
-    int k = params[2];
+    int j = params[2];
     params.erase(params.begin());
     int seq_size = params.size();
 
@@ -217,27 +312,32 @@ int additive_method(int n, std::vector<int> params, std::string f)
 }
 
 
+std::bitset<BIT_AMOUNT> lfsr_iteration(std::bitset<BIT_AMOUNT> init_register,
+                                       std::bitset<BIT_AMOUNT> poly_coeffs)
+{
+    bool current_bit = 0;
+    for (int j = 0; j < BIT_AMOUNT; j++)
+    {
+        current_bit ^= init_register[j] * poly_coeffs[j];
+    }
+    init_register >>= 1;
+    init_register[BIT_AMOUNT - 1] = current_bit;
+    return init_register;
+}
+
+
 int lfsr_method(int n, std::vector<std::string> str_init, std::string f)
 {
     // реализация РСЛОС метода
-    const int bit_amount = 32;
-    std::bitset<bit_amount> init_register(str_init[0]);
-    std::bitset<bit_amount> poly_coeffs(str_init[1]);
+    std::bitset<BIT_AMOUNT> poly_coeffs(str_init[0]);
+    std::bitset<BIT_AMOUNT> init_register(str_init[1]);
 
     std::ofstream output_file;
     output_file.open(f);
     for (int i = 0; i < n; i++)
     {
         output_file << init_register.to_ulong() << ',';
-
-        bool current_bit = 0;
-        for (int j = 0; j < bit_amount; j++)
-        {
-            current_bit ^= init_register[j] * poly_coeffs[j];
-        }
-        init_register >>= 1;
-        init_register[bit_amount - 1] = current_bit;
-
+        init_register = lfsr_iteration(init_register, poly_coeffs);
         show_progress(i, n);
     }
     output_file.close();
@@ -248,11 +348,11 @@ int lfsr_method(int n, std::vector<std::string> str_init, std::string f)
 int five_param_method(int n, std::vector<int> str_init, std::string f)
 {
     // реализация пятипараметрического метода
-    int q_1 = str_init[0];
-    int q_2 = str_init[1];
-    int q_3 = str_init[2];
-    int w = str_init[3];
-    int p = str_init[4];
+    int p = str_init[0];
+    int q_1 = str_init[1];
+    int q_2 = str_init[2];
+    int q_3 = str_init[3];
+    int w = str_init[4];
     
     boost::dynamic_bitset<> init_register(w, p);
 
@@ -272,6 +372,82 @@ int five_param_method(int n, std::vector<int> str_init, std::string f)
 
         show_progress(i, n);
     }
+    output_file.close();
+    return 0;
+}
+
+// 00000000000000000000000000000110
+// 00000000000000000000000000000001
+// 00000000000000000000000000000100
+// 00000000000000000000000000000001
+// 00000000000000000000000000000010
+// 00000000000000000000000000000001
+
+int nfsr_method(int n, std::vector<std::string> str_init, std::string f)
+{
+    std::bitset<BIT_AMOUNT> pc_1(str_init[0]);
+    std::bitset<BIT_AMOUNT> ir_1(str_init[1]);
+    std::bitset<BIT_AMOUNT> pc_2(str_init[2]);
+    std::bitset<BIT_AMOUNT> ir_2(str_init[3]);
+    std::bitset<BIT_AMOUNT> pc_3(str_init[4]);
+    std::bitset<BIT_AMOUNT> ir_3(str_init[5]);
+    std::bitset<BIT_AMOUNT> cur_reg;
+
+    std::ofstream output_file;
+    output_file.open(f);
+    for (int i = 0; i < n; i++)
+    {
+        cur_reg = (ir_1 & ir_2) ^ (ir_2 & ir_3) ^ ir_3;
+        output_file << cur_reg.to_ulong() << ',';
+
+        ir_1 = lfsr_iteration(ir_1, pc_1);
+        ir_2 = lfsr_iteration(ir_2, pc_2);
+        ir_3 = lfsr_iteration(ir_3, pc_3);
+
+        show_progress(i, n);
+    }
+    output_file.close();
+    return 0;
+}
+
+
+int mt_method()
+{
+    return 0;
+}
+
+
+int rc4_method(int n, std::vector<int> str_init, std::string f)
+{
+    std::vector<int> s_block;
+    for (int i = 0; i < 256; i++)
+    {
+        s_block.push_back(i);
+    }
+    int j = 0;
+    for (int i = 0; i < 256; i++)
+    {
+        j = (j + s_block[i] + str_init[i]) % 256;
+        std::swap(s_block[i], s_block[j]);
+    }
+
+    std::ofstream output_file;
+    output_file.open(f);
+    
+    int i = 0;
+    j = 0;
+    for (int l = 0; l < n; l++)
+    {
+        i = (i + 1) % 256;
+        j = (j + s_block[i]) % 256;
+        std::swap(s_block[i], s_block[j]);
+        int t = (s_block[i] + s_block[j]) % 256;
+        int k = s_block[t];
+
+        output_file << k << ',';
+        show_progress(i, n);
+    }
+    
     output_file.close();
     return 0;
 }
@@ -322,7 +498,7 @@ void parse_args(std::string &g, std::vector<int> &init, int &n, std::string &f,
                 }
             }
         }
-        if (g == "lfsr")
+        if (g == "lfsr" or g == "nfsr")
         {
             str_init = split_to_str(pre_init, ",");
         }
@@ -354,6 +530,7 @@ int main(int argc, char** argv)
 
     if (g != "" && (!init.empty() || !str_init.empty()))
     {
+        int generation_status = 0;
         if (h)
         {
             show_help_message(g, h);
@@ -361,26 +538,39 @@ int main(int argc, char** argv)
         std::cout << "Генерация выполнена на ";
         if (g == "lc")
         {
-            linear_congruent_method(n, init, f);
+            generation_status = linear_congruent_method(n, init, f);
         }
         else if (g == "add")
         {
-            additive_method(n, init, f);
+            generation_status = additive_method(n, init, f);
         }
         else if (g == "lfsr")
         {
-            lfsr_method(n, str_init, f);
+            generation_status = lfsr_method(n, str_init, f);
         }
         else if (g == "5p")
         {
-            five_param_method(n, init, f);
+            generation_status = five_param_method(n, init, f);
         }
-        std::cout << "\r" << "Генерация выполнена на " << 100 << "%" << std::flush 
-              << std::endl;
-        std::cout << "Результат работы генератора сохранен в " + f + "\n";
+        else if (g == "nfsr")
+        {
+            generation_status = nfsr_method(n, str_init, f);
+        }
+        else if (g == "rc4")
+        {
+            generation_status = rc4_method(n, init, f);
+        }
+        if (!generation_status)
+        {
+            std::cout << "\r" << "Генерация выполнена на " << 100 << "%" << std::flush 
+                << std::endl;
+            std::cout << "Результат работы генератора сохранен в " + f + "\n";
+        }        
     }
     else
     {
         show_help_message(g, h);
     }
 }
+
+// 1,2,3,4,5,6,7,8,1,2,3,4,5,6,7,8,1,2,3,4,5,6,7,8,1,2,3,4,5,6,7,8,1,2,3,4,5,6,7,8,1,2,3,4,5,6,7,8,1,2,3,4,5,6,7,8,1,2,3,4,5,6,7,8,1,2,3,4,5,6,7,8,1,2,3,4,5,6,7,8,1,2,3,4,5,6,7,8,1,2,3,4,5,6,7,8,1,2,3,4,5,6,7,8,1,2,3,4,5,6,7,8,1,2,3,4,5,6,7,8,1,2,3,4,5,6,7,8,1,2,3,4,5,6,7,8,1,2,3,4,5,6,7,8,1,2,3,4,5,6,7,8,1,2,3,4,5,6,7,8,1,2,3,4,5,6,7,8,1,2,3,4,5,6,7,8,1,2,3,4,5,6,7,8,1,2,3,4,5,6,7,8,1,2,3,4,5,6,7,8,1,2,3,4,5,6,7,8,1,2,3,4,5,6,7,8,1,2,3,4,5,6,7,8,1,2,3,4,5,6,7,8,1,2,3,4,5,6,7,8,1,2,3,4,5,6,7,8,1,2,3,4,5,6,7,8
